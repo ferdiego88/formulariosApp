a0116d7634187e21d9056bcf79669ac464131491
Fernando Santillan <ferdiego88@gmail.com>
2022-07-07 22:14:52 -0500

    fsv Menus SideBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
+    {
+      path: 'template',
+      loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)
+    },
 
+    {
+      path: 'reactive',
+      loadChildren: () => import('./reactive/reactive.module').then(m => m.ReactiveModule)
+    },
+    {
+      path: '**',
+      redirectTo: 'template'
+    }
 ];
 
 @NgModule({
