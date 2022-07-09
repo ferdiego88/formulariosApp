717b1e032db1c837ff0535e609c3e7eb7ed46b79
Fernando Santillan <ferdiego88@gmail.com>
2022-07-08 21:23:50 -0500

    fsv eliminar elementos Dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styleUrls: ['./dinamicos.component.css']
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent   {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Fernando',
+    favoritos: [
+      {id: 1, nombre: 'Metal Gear'},
+      {id: 2, nombre: 'Blody Roar'},
+    ]
   }
 
+  guardar(){
+    console.log('formulario posteado');
+
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index);
+  }
 }
