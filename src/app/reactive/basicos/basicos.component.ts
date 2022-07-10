ee972627a03768ef07927e0572a458986fd07298
Fernando Santillan <ferdiego88@gmail.com>
2022-07-09 20:17:10 -0500

    fsv init Formularios Reactivos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent  {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre      : new FormControl('Rtx 4080ti'),
+  //   precio      : new FormControl(250),
+  //   existencias : new FormControl(70)
+  // })
 
-  ngOnInit(): void {
-  }
+  miFormulario: FormGroup = this.formBuilder.group({
+      nombre: ['Rtx 4080ti',[Validators.required,Validators.minLength(3)],],
+      precio: [250,[Validators.required,Validators.min(0)]],
+      existencias: [70,[Validators.required,Validators.min(0)]],
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
+
 
   guardar() {
     console.log('Submit Hecho');
