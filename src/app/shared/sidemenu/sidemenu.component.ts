c551e8d2451d68f287848d04514dcb47c914029c
Fernando Santillan <ferdiego88@gmail.com>
2022-07-10 11:19:55 -0500

    fsv Rutas LazyLoad

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -41,5 +41,16 @@ export class SidemenuComponent {
     },
   ]
 
+  authMenu: MenuItem[] = [
+    {
+      texto: 'Registro',
+      ruta: './auth/registro'
+    },
+    {
+      texto: 'Login',
+      ruta: './auth/login'
+    },
+  ]
+
 
 }
