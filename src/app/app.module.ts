8d9749cadc8eb374a7a41a53fe99cedb3aaf40b9
Fernando Santillan <ferdiego88@gmail.com>
2022-07-14 19:41:03 -0500

    validaciones reactivas Email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -12,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorService } from '../../../shared/validators/validator.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
+
+
 
 @Component({
   selector: 'app-registro',
@@ -11,7 +14,7 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['',[Validators.required,Validators.pattern(this.validatorService.nombreApellidoPatern)]],
-    email: ['',[Validators.required,Validators.pattern(this.validatorService.emailPattern)]],
+    email: ['',[Validators.required,Validators.pattern(this.validatorService.emailPattern)],[this.emailValidator]],
     usuario: ['',[Validators.required,this.validatorService.noPuedeSerFerdiego],],
     password: ['',[Validators.required, Validators.minLength(6)]],
     password2: ['',[Validators.required]]
@@ -19,14 +22,27 @@ export class RegistroComponent implements OnInit {
     validators: [this.validatorService.camposIguales('password','password2')]
   })
 
+
+  get emailErrorMsg():string {
+    const errors = this.miFormulario.get('email')?.errors;
+    if (errors?.['required']) {
+        return 'Correo electrónico es obligatorio';
+    } else if(errors?.['pattern']) {
+      return 'El formato de correo electrónico es inválido'
+    } else if(errors?.['emailTomado']) {
+      return 'El email ingresado esta siendo utilizado'
+    }
+    return '';
+  }
   constructor(private fb: FormBuilder,
-              private validatorService: ValidatorService) { }
+              private validatorService: ValidatorService,
+              private emailValidator: EmailValidatorService) { }
 
   ngOnInit(): void {
 
     this.miFormulario.reset({
       nombre: 'Fernando Herrera',
-      email: 'fernandoherrera@gmail.com'
+      email: 'test1@test.com'
     })
   }
 
@@ -40,4 +56,21 @@ export class RegistroComponent implements OnInit {
 
     this.miFormulario.markAllAsTouched();
   }
+
+  // emailRequired(){
+  //   return this.miFormulario.get('email')?.touched &&
+  //      this.miFormulario.get('email')?.errors?.['required'];
+  // }
+
+
+  // formatoEmail(){
+  //   return this.miFormulario.get('email')?.touched &&
+  //      this.miFormulario.get('email')?.errors?.['pattern'];
+  // }
+
+  // emailTomado(){
+  //   return this.miFormulario.get('email')?.touched &&
+  //      this.miFormulario.get('email')?.errors?.['emailTomado'];
+  // }
+
 }
